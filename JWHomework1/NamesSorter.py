c40ddb414dbaedcd0a4296dd9728d820a95ce087
NovaZure <jaytonward@yahoo.com>
2022-03-23 20:16:21 -0400

    Initial commit

---
+++ JWHomework1/NamesSorter.py
@@ -0,0 +1,42 @@
+
+import re
+
+#TODO: Use a more efficient sort than selection sort
+def mySort(data):
+    #First sort the list alphabetically
+    data.sort()
+
+    #Use selection sort to sort by length
+    for x in range(len(data)):
+        min = x;
+
+        for y in range(x+1, len(data)):
+            if (len(data[y]) < len(data[min])):
+                min = y
+
+        #Instead of swapping the values, the displaced element is inserted at the
+        #front of the unsorted partition to retain the alphabetical sorting
+        if (min != x):
+            temp = data[x]
+            data[x] = data.pop(min)
+            data.insert(x+1, temp)
+
+    return data
+
+
+data = []
+
+#TODO: Add check to see if file exists
+with open('Sort Me.txt') as f:
+    #Add all of the data to a list
+    for line in f:
+        #Cut off any spaces, numbers, or newline characters
+        x = re.search("([A-Z]|[a-z]){2,}", line)
+        if (x and x.group()):
+            data.append(x.group())
+
+
+data = mySort(data)
+
+for x in data:
+    print(x)
